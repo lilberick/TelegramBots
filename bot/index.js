02a035b40b3949d5b60c7c5cb12f261c6fe32cbf
lilberick <b_lil_berick@hotmail.com>
2020-04-28 22:59:55 -0500

    version 0.1
    El bot sube una imagen con los fallecidos por coronavirus en cada departamento del Perú

---
+++ bot/index.js
@@ -0,0 +1,13 @@
+const TelegramBot = require( "node-telegram-bot-api" );
+const token = 'GGGGAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
+const bot = new TelegramBot(token, {polling: true});
+
+bot.onText(/\/covid/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'Cargando mapa...');
+  bot.sendPhoto(chatId,"../img/PeruTemporal.jpg",{
+	  caption	: "Perú: Muertes por coronavirus"
+  })
+  console.log("Foto enviada");
+});
+
---
+++ mapa.py
@@ -0,0 +1,21 @@
+from PIL import Image, ImageDraw, ImageFont
+import csv
+imagen = Image.open('img/Peru.jpg')
+dibujo = ImageDraw.Draw(imagen)
+fuente = ImageFont.truetype('FreeMonoBold.ttf', size=20)
+color = 'rgb(237, 28, 36)'
+with open('csv/datos.csv', mode='r') as infile:
+    reader = csv.reader(infile)
+    ejex = {rows[0]:rows[1] for rows in reader}
+with open('csv/datos.csv', mode='r') as infile:
+    reader = csv.reader(infile)
+    ejey = {rows[0]:rows[2] for rows in reader}
+with open('csv/datos.csv', mode='r') as infile:
+    reader = csv.reader(infile)
+    valor = {rows[0]:rows[5] for rows in reader}
+for (i,j,v) in zip(ejex,ejey,valor):
+    (x,y) = (int(ejex[i]),int(ejey[j]))
+    dibujo.text((x, y),valor[v], fill=color, font=fuente)
+imagen.save('img/PeruTemporal.jpg')
+
+
