9caee35c38adb0eebf74b64bbda800e8766c07f1
lilberick <b_lil_berick@hotmail.com>
2020-05-11 21:39:45 -0500

    actualizacion de selector

--- tabla.R
+++ tabla.R
@@ -2,12 +2,13 @@ library(rvest)
 url <- "https://es.wikipedia.org/wiki/Pandemia_de_enfermedad_por_coronavirus_de_2020_en_Per%C3%BA"
 pagina_web <- read_html(url)
 class(pagina_web)
-selector <- "#mw-content-text > div > table:nth-child(104)"
+selector <- "#mw-content-text > div > table:nth-child(107) > tbody > tr > td:nth-child(1) > table"
 nodo <- html_node(pagina_web,selector)
-nodo_tabla <- html_table(nodo)
-guardar <- nodo_tabla[1:25,1:3]
+nodo_tabla <- html_table(nodo,fill=TRUE)
+guardar <- nodo_tabla[2:26,1:3]
 a <- read.csv("csv/coordenadas.csv")
 guardar <- data.frame(a,guardar)
 #write.csv(guardar,"datos.csv",row.names=F)
 write.table(guardar,"csv/datos.csv",sep=",",row.names=F,col.names=F)
 guardar
+
