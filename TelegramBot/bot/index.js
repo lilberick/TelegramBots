d04a0861b632857f65dcd25f0950e989b72ca5a8
lilberick <lilberick@gmail.com>
2020-07-03 22:25:50 -0500

    Agregando menú keyboard al bot

--- TelegramBot/bot/index.js
+++ TelegramBot/bot/index.js
@@ -1,57 +1,65 @@
 const TelegramBot = require( "node-telegram-bot-api" );
-const token = 'GGAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
+const token = 'GAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
 const bot = new TelegramBot(token, {polling: true});
-//Muertos
-bot.onText(/\/muertos/, (msg) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'Cargando mapa...');
-  bot.sendPhoto(chatId,"/home/pi/app/TelegramBotCovid/TelegramBot/img/Muertos.png",{
-  //bot.sendPhoto(chatId,"../img/Muertos.png",{
-	  caption	: "Perú: Muertes por coronavirus"
-  })
-  console.log("Foto enviada");
+//Menu
+bot.onText(/\/covid/, (msg) => {
+bot.sendMessage(msg.chat.id, "Bienvenido, soy covidPeruBot", {
+"reply_markup": {
+    "keyboard": [["Confirmados", "Muertos","Poblacion"], ["Audio","Video"]]
+    }
+});
 });
 
-//Confirmados
-bot.onText(/\/confirmados/, (msg) => {
+//Respuestas
+bot.on('message', (msg) => {
+var a = "Confirmados";
+if (msg.text.indexOf(a) === 0) {
   const chatId = msg.chat.id;
   bot.sendMessage(chatId, 'Cargando mapa...');
-  bot.sendPhoto(chatId,"/home/pi/app/TelegramBotCovid/TelegramBot/img/Confirmados.png",{
-  //bot.sendPhoto(chatId,"../img/Confirmados.png",{
+  //bot.sendPhoto(chatId,"/home/pi/app/TelegramBotCovid/TelegramBot/img/Confirmados.png",{
+  bot.sendPhoto(chatId,"../img/Confirmados.png",{
 	  caption	: "Perú: Casos confirmados de Covid"
   })
   console.log("Foto enviada");
-});
-
-//Poblacion
-bot.onText(/\/poblacion/, (msg) => {
+}
+var b = "Muertos";
+if (msg.text.indexOf(b) === 0) {
   const chatId = msg.chat.id;
   bot.sendMessage(chatId, 'Cargando mapa...');
-  bot.sendPhoto(chatId,"/home/pi/app/TelegramBotCovid/TelegramBot/img/Poblacion.png",{
-  //bot.sendPhoto(chatId,"../img/Poblacion.png",{
+  //bot.sendPhoto(chatId,"/home/pi/app/TelegramBotCovid/TelegramBot/img/Muertos.png",{
+  bot.sendPhoto(chatId,"../img/Muertos.png",{
+	  caption	: "Perú: Muertes por coronavirus"
+  })
+  console.log("Foto enviada");
+}
+var c = "Poblacion";
+if (msg.text.indexOf(c) === 0) {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'Cargando mapa...');
+  //bot.sendPhoto(chatId,"/home/pi/app/TelegramBotCovid/TelegramBot/img/Poblacion.png",{
+  bot.sendPhoto(chatId,"../img/Poblacion.png",{
 	  caption	: "Perú: Población actual"
   })
   console.log("Foto enviada");
-});
-
-//Audio
-bot.onText(/\/audio/, (msg) => {
+}
+var d= "Audio";
+if (msg.text.indexOf(d) === 0) {
   const chatId = msg.chat.id;
   bot.sendMessage(chatId, 'Cargando audio...');
-  bot.sendAudio(chatId,"/home/pi/app/TelegramBotCovid/TelegramBot/img/Coronavairus.mp3",{
-  //bot.sendAudio(chatId,"../img/Coronavairus.mp3",{
+  //bot.sendAudio(chatId,"/home/pi/app/TelegramBotCovid/TelegramBot/img/Coronavairus.mp3",{
+  bot.sendAudio(chatId,"../img/Coronavairus.mp3",{
 	  caption	: "Coronavairus audio"
   })
   console.log("audio enviado");
-});
-
-//Video
-bot.onText(/\/video/, (msg) => {
+}
+var e= "Video";
+if (msg.text.indexOf(e) === 0) {
   const chatId = msg.chat.id;
   bot.sendMessage(chatId, 'Cargando video...');
-  bot.sendVideo(chatId,"/home/pi/app/TelegramBotCovid/TelegramBot/img/Coronavairus.mp4",{
-  //bot.sendVideo(chatId,"../img/Coronavairus.mp4",{
+  //bot.sendVideo(chatId,"/home/pi/app/TelegramBotCovid/TelegramBot/img/Coronavairus.mp4",{
+  bot.sendVideo(chatId,"../img/Coronavairus.mp4",{
 	  caption	: "Coronavairus video"
   })
   console.log("Video enviado");
+}
 });
